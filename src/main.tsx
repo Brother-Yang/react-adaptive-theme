50a8bcd893337e8e3ff31839b22d94ef0fce8c00
lisuyang <450028105@qq.com>
2025-09-19 10:36:05 +0800

    refactor(系统配置): 替换AppWithTheme为SystemConfigProvider组件

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import '@ant-design/v5-patch-for-react-19';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from './contexts/ThemeContext';
-import AppWithTheme from './components/AppWithTheme';
+import SystemConfigProvider from '@/components/System/SystemConfigProvider';
 import './config/i18n'; // 初始化i18n配置
 import './index.less';
 import { performanceMonitor } from './utils/performance.ts';
@@ -15,7 +15,7 @@ if (import.meta.env.DEV) {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <AppWithTheme />
+      <SystemConfigProvider />
     </ThemeProvider>
   </StrictMode>
 );
